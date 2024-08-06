5c6223845155d8df541d1fd2c19207805ea65147
jrivas <josuerivas2311@gmail.com>
2024-08-05 19:13:59 -0600

    Handle errors

--- src/presentation/auth/controllers.ts
+++ src/presentation/auth/controllers.ts
@@ -1,8 +1,16 @@
-import e, { Request, Response } from 'express';
-import { AuthRepository, RegisterUserDto } from '../../domain';
+import { Request, Response } from 'express';
+import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
 
 export class AuthController {
   constructor(private readonly authRepository: AuthRepository) {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError)
+      return res.status(error.statusCode).json({ error: error.message });
+
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  };
 
   registerUser = (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create(req.body);
@@ -12,7 +20,7 @@ export class AuthController {
     this.authRepository
       .register(registerUserDto!)
       .then((user) => res.json(user))
-      .catch((error) => res.status(500).json(error));
+      .catch((error) => this.handleError(error, res));
   };
 
   loginUser = (req: Request, res: Response) => {
